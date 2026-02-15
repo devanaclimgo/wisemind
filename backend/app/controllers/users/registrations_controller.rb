0c801510e7acfa716319e42f06fcb4fb7b3b09a9
Ana <anaclimgo@gmail.com>
2026-02-15 18:18:23 -0300

    fix: created sign up function that doesn't sign in the user after sign up to prevent the JWT from being dispatched immediately after registration

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -3,6 +3,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def sign_up(resource_name, resource)
+     # Don't sign in the user after sign up
+     # This is important to prevent the JWT from being dispatched immediately after registration
+     # and allows the client to handle the login process separately if needed.
+  end
+
   def respond_with(resource, _opts = {})
     if resource.persisted?
       render json: {
@@ -11,7 +17,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       }, status: :ok
     else
       render json: {
-        message: "User couldn't be created successfully",
+        message: "User couldn't be created",
         errors: resource.errors.full_messages
       }, status: :unprocessable_entity
     end
