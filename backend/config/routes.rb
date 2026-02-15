5b458308250f4435c4e9f9829932cd491e79fa07
Ana <anaclimgo@gmail.com>
2026-02-15 18:25:31 -0300

    feat: added weeks route from api v1

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :weeks, only: [:index, :show, :create]
+    end
+  end
 end
