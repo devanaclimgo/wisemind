54e5d3feb68251552467d430ddde7c193fade5a8
Ana <anaclimgo@gmail.com>
2026-02-15 18:24:16 -0300

    refactor: week model to generate day entries

--- backend/app/models/week.rb
+++ backend/app/models/week.rb
@@ -1,3 +1,18 @@
 class Week < ApplicationRecord
   belongs_to :user
+  has_many :day_entries, dependent: :destroy
+
+  after_create :generate_days
+
+  validates :start_date, presence: true
+
+  private
+
+  def generate_days
+    7.times do |i|
+      day_entries.create!(
+        day_number: i + 1
+      )
+    end
+  end
 end
