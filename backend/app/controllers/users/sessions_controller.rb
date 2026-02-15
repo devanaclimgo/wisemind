232dbd424aea6dab72eaed9b5360bf2ecab09924
Ana <anaclimgo@gmail.com>
2026-02-15 16:49:02 -0300

    feat: added session configuration to log in and log out successfully

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,2 +1,18 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+      message: 'Logged in successfully',
+      user: resource
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    render json: {
+      message: 'Logged out successfully'
+    }, status: :ok
+  end
 end
