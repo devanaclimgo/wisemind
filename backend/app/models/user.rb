1ed1239daff97e569fc908425d3774e37647213c
Ana <anaclimgo@gmail.com>
2026-02-22 20:24:41 -0300

    feat: created login option for username and/or email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
          jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
   has_many :weeks, dependent: :destroy
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value",
+      { value: login.downcase }]
+    ).first
+  end
 end
