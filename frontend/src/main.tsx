336464497816f805ebfdce30ae6baf8151305d8f
Ana <anaclimgo@gmail.com>
2026-02-22 19:48:20 -0300

    feat: added create week and week details routes to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dahboard";
 import Signup from "./pages/SignUp";
+import CreateWeek from "./pages/CreateWeek";
+import WeekDetails from "./pages/WeekDetails";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -11,6 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/weeks/new" element={<CreateWeek />} />
+      <Route path="/weeks/:id" element={<WeekDetails />} />
     </Routes>
   </BrowserRouter>
 );
