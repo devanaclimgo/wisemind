4e2c721c85bcbc5c1a710a6cd02c67847c0dfb06
Ana <anaclimgo@gmail.com>
2026-02-25 17:52:30 -0300

    refactor: responde_with now has proper error rendering and added respond_to_on_create in case of invalid email/username or password

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -3,10 +3,9 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_to_on_destroy
     render json: {
-      message: 'Logged in successfully',
-      user: resource
+      message: 'Logged out successfully'
     }, status: :ok
   end
 
@@ -14,5 +13,26 @@ class Users::SessionsController < Devise::SessionsController
     render json: {
       message: 'Logged out successfully'
     }, status: :ok
+  end
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        message: 'Logged in successfully',
+        user: resource
+      }, status: :ok
+    else
+      render json: {
+        error: 'Invalid email/username or password'
+      }, status: :unauthorized
+    end
+  end
+
+  def respond_to_on_create
+    if current_user
+      render json: { user: current_user }, status: :ok
+    else
+      render json: { error: "Invalid email/username or password" }, status: :unauthorized
+    end
   end
 end
