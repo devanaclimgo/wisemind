25676b44c9e8943c36b036cc2ffd411546432a7b
Ana <anaclimgo@gmail.com>
2026-02-25 19:29:39 -0300

    feat: implemented error boundary class inside browser router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,19 @@ import Dashboard from "./pages/Dahboard";
 import Signup from "./pages/SignUp";
 import CreateWeek from "./pages/CreateWeek";
 import WeekDetails from "./pages/WeekDetails";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
+    <ErrorBoundary>
+      <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/weeks/new" element={<CreateWeek />} />
       <Route path="/weeks/:id" element={<WeekDetails />} />
     </Routes>
+    </ErrorBoundary>
   </BrowserRouter>
 );
