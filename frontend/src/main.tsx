0668c3353e614662f942220f7bc5c37bd70b1ef3
Ana <anaclimgo@gmail.com>
2026-02-26 16:05:38 -0300

    feat: added homepage route to main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,18 +6,20 @@ import Signup from "./pages/SignUp";
 import CreateWeek from "./pages/CreateWeek";
 import WeekDetails from "./pages/WeekDetails";
 import ErrorBoundary from "./components/ErrorBoundary";
+import Home from "./pages/Home";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ErrorBoundary>
       <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/weeks/new" element={<CreateWeek />} />
-      <Route path="/weeks/:id" element={<WeekDetails />} />
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/weeks/new" element={<CreateWeek />} />
+        <Route path="/weeks/:id" element={<WeekDetails />} />
+      </Routes>
     </ErrorBoundary>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
