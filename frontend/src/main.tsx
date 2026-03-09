e0d35972044b46fd0ed89e526b7b2bff79f81c87
Ana <anaclimgo@gmail.com>
2026-03-09 17:45:14 -0300

    fix: file name from dahboard to dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dahboard";
+import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/SignUp";
 import CreateWeek from "./pages/CreateWeek";
 import WeekDetails from "./pages/WeekDetails";
