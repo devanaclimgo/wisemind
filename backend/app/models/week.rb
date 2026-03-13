a34222270ce9813e35f7b90d30926350827233e0
Ana <anaclimgo@gmail.com>
2026-03-13 11:18:43 -0300

    refactor: has many day entried now has both day number order and dependent destroy in a single line, created end date function

--- backend/app/models/week.rb
+++ backend/app/models/week.rb
@@ -1,14 +1,16 @@
 class Week < ApplicationRecord
   belongs_to :user
-  has_many :day_entries, dependent: :destroy
-  has_many :day_entries, -> { order(:day_number) }
+  has_many :day_entries, -> { order(:day_number) }, dependent: :destroy
 
   # TODO: adicionar ordenação por criação do card, para aparecer o mais recente primeiro
-  has_many :weeks, -> { order(:created_at) }
 
   after_create :generate_days
 
   validates :start_date, presence: true
+
+  def end_date
+    start_date + 6.days
+  end
 
   private
 
