0a95c4f99b3407a3fc9c9a97c0d21ff262f39f59
Ana <anaclimgo@gmail.com>
2026-05-04 18:47:48 -0300

    feat: add update action to weeks resource in API routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :weeks, only: [:index, :show, :create, :destroy]
+      resources :weeks, only: [:index, :show, :create, :update, :destroy]
       resources :day_entries, only: [:update]
     end
   end
